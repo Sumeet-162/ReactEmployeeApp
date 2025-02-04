8ec128e597ae247ffffc63144856be4de7a9af35
Sumeet <117519961+Sumeet-162@users.noreply.github.com>
2025-02-04 18:07:56 +0530

    Update AllTask.jsx

--- src/components/other/AllTask.jsx
+++ src/components/other/AllTask.jsx
@@ -3,29 +3,31 @@ import { AuthContext } from '../../context/AuthProvider'
 
 const AllTask = () => {
 
-  const [userData,setUserData] = useContext(AuthContext)
-  
+   const [userData,setUserData] =  useContext(AuthContext)
 
+   
   return (
-    <div id="tasklist" className='bg-[#1C1C1C] p-3 mt-5 rounded '>
-      <div className='bg-red-400 mt-2  px-4 py-2 flex justify-between rounded'>
-        <h2 className='w-1/5 bg-red-400 px-1 rounded-l'>Employee Name</h2>
-        <h3 className='w-1/5 bg-red-400' >New Task</h3>
-        <h5 className='w-1/5 bg-red-400'>Active Task</h5>
-        <h5 className='w-1/5 bg-red-400'>Completed Task</h5>
-        <h5 className='w-1/5 bg-red-400 rounded-r'>Failed Task</h5>
-      </div>
-      <div  className=''>
-      {userData.map(function(elem,idx){
-        return <div key={idx} className=' border-2 border-white mt-2  px-4 py-2 flex justify-between rounded'>
-        <h2 className='w-1/5 px-1 rounded-l'>{elem.firstName}</h2>
-        <h3 className='w-1/5' >{elem.taskNumbers.newTask}</h3>
-        <h5 className='w-1/5 '>{elem.taskNumbers.active}</h5>
-        <h5 className='w-1/5'>{elem.taskNumbers.completed}</h5>
-        <h5 className='w-1/5 rounded-r '>{elem.taskNumbers.failed}</h5>
-      </div>
-      })}
-      </div>
+    <div className='bg-[#1c1c1c] p-5 rounded mt-5'>
+        <div className='bg-red-400 mb-2 py-2 px-4 flex justify-between rounded'>
+            <h2 className='text-lg font-medium w-1/5'>Employee Name</h2>
+            <h3 className='text-lg font-medium w-1/5'>New Task</h3>
+            <h5 className='text-lg font-medium w-1/5'>Active Task</h5>
+            <h5 className='text-lg font-medium w-1/5'>Completed</h5>
+            <h5 className='text-lg font-medium w-1/5'>Failed</h5>
+        </div>
+        <div className=''>
+        {userData.map(function(elem,idx){
+            return <div key={idx} className='border-2 border-emerald-500 mb-2 py-2 px-4 flex justify-between rounded'>
+            <h2 className='text-lg font-medium  w-1/5'>{elem.firstName}</h2>
+            <h3 className='text-lg font-medium w-1/5 text-blue-400'>{elem.taskCounts.newTask}</h3>
+            <h5 className='text-lg font-medium w-1/5 text-yellow-400'>{elem.taskCounts.active}</h5>
+            <h5 className='text-lg font-medium w-1/5 text-white'>{elem.taskCounts.completed}</h5>
+            <h5 className='text-lg font-medium w-1/5 text-red-600'>{elem.taskCounts.failed}</h5>
+        </div>
+        })}
+        </div>
+        
+        
     </div>
   )
 }
