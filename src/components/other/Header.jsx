39965c29cc0561c4b201dc5b7558e47c104b108d
Sumeet <117519961+Sumeet-162@users.noreply.github.com>
2025-02-04 18:08:47 +0530

    Update Header.jsx

--- src/components/other/Header.jsx
+++ src/components/other/Header.jsx
@@ -1,20 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { setLocalStorage } from '../../utils/localStorage'
-import { getLocalStorage } from '../../utils/localStorage'
 
 const Header = (props) => {
 
-  
-  const logOutUser=()=>{
+  // const [username, setUsername] = useState('')
+
+  // if(!data){
+  //   setUsername('Admin')
+  // }else{
+  //   setUsername(data.firstName)
+  // }
+
+  const logOutUser = ()=>{
     localStorage.setItem('loggedInUser','')
     props.changeUser('')
-    //window.location.reload()
+    // window.location.reload()
   }
 
+  
   return (
-    <div className='flex items-end justify-between rounded-xl'>
-      <h1 className='text-2xl text-gray-50 m-2.5 '>Hello <span className='text-3xl font-semibold'>👋</span></h1>
-      <button onClick={logOutUser} className='bg-yellow-400 text-black text-lg font-medium px-5 py-2 rounded-sm m-5'>Log Out</button>
+    <div className='flex items-end justify-between'>
+        <h1 className='text-2xl font-medium'>Hello <br /> <span className='text-3xl font-semibold'>username 👋</span></h1>
+        <button onClick={logOutUser} className='bg-red-600 text-base font-medium text-white px-5 py-2 rounded-sm'>Log Out</button>
     </div>
   )
 }
