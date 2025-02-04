9cadde508c39574d261da65a523d49fac2028c5b
Sumeet <117519961+Sumeet-162@users.noreply.github.com>
2025-02-04 18:05:55 +0530

    Update AcceptTask.jsx

--- src/components/TaskList/AcceptTask.jsx
+++ src/components/TaskList/AcceptTask.jsx
@@ -1,19 +1,22 @@
 import React from 'react'
 
 const AcceptTask = ({data}) => {
+    console.log();
   return (
-    <div className='flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl p-5'>
-        <div className='flex justify-between items-center'>
-          <h3 className='bg-red-600 px-3 py-1 text-xs rounded'>{data.category}</h3>
-          <h4>{data.date}</h4>
+    <div className='flex-shrink-0 h-full w-[300px] p-5 bg-red-400 rounded-xl'>
+            <div className='flex justify-between items-center'>
+                <h3 className='bg-red-600 text-sm px-3 py-1 rounded'>{data.category}</h3>
+                <h4 className='text-sm'>{data.taskDate}</h4>
+            </div>
+            <h2 className='mt-5 text-2xl font-semibold'>{data.taskTitle}</h2>
+            <p className='text-sm mt-2'>
+                {data.taskDescription}
+            </p>
+            <div className='flex justify-between mt-6 '>
+                <button className='bg-green-500 rounded font-medium py-1 px-2 text-xs'>Mark as Completed</button>
+                <button className='bg-red-500 rounded font-medium py-1 px-2 text-xs'>Mark as Failed</button>
+            </div>
         </div>
-        <h2 className='mt-4 text-xl font-semibold text-black'>{data.title}</h2>
-        <p className='text-sm mt-2'>{data.description}</p>
-        <div className='flex justify-between'>
-          <button className='bg-green-500 py-1 px-2 text-sm rounded mt-4'>Completed</button>
-          <button className='bg-red-500 py-1 px-2 text-sm rounded mt-4'>Failed</button>
-        </div>
-      </div>
   )
 }
 
